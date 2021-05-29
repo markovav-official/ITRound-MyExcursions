a62b266733d336c1d31df7f0463701c798456415
Markov Andrei <38224264+markovav-official@users.noreply.github.com>
2021-05-29 19:28:49 +0500

    Fix start_uwsgi_server.sh

--- start_uwsgi_server.sh
+++ start_uwsgi_server.sh
@@ -1,1 +1,1 @@
-uwsgi --enable-threads --socket excursions-markovav-ru.sock --wsgi-file excursions-markovav-ru/wsgi.py --chmod-socket=666 --workers 1
+uwsgi --enable-threads --socket excursions-markovav-ru.sock --wsgi-file excursions_markovav_ru/wsgi.py --chmod-socket=666 --workers 1
